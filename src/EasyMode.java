361922ebebde0fcd7be584d9212ffdca8d3ab151
BT_4N2_06 <tzhao9389@bths.edu>
2023-05-03 12:21:52 -0400

    new

--- src/EasyMode.java
+++ src/EasyMode.java
@@ -2,11 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class EasyMode extends JFrame{
-    private JButton button1;
     private JPanel easyPanel;
+    private JButton button1;
 
+ public JPanel getEasyPanel(){
+     return easyPanel;
+ }
 
-    public JPanel getEasyPanel(){
-        return easyPanel;
-    }
 }
---
+++ src/HardMode.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HardMode extends JFrame{
+    private JPanel hardMode;
+    private JButton backButton;
+}
