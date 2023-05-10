932e0a142e61568e8aeb20cc61c31cca1a210a4e
BT_4N2_06 <tzhao9389@bths.edu>
2023-05-10 12:18:04 -0400

    new

--- src/EasyMode.java
+++ src/EasyMode.java
@@ -1,12 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class EasyMode extends JFrame{
-    private JPanel easyPanel;
-    private JButton button1;
 
+public class EasyMode extends JFrame {
+    private JPanel easyPanel;
+    private JButton quit;
+
+public EasyMode(){
+    easyPanel.setLayout(null);
+    quit.setBounds(0,0,50,20);
+  quit.setText("Main Menu");
+    setContentPane(easyPanel);
+    System.out.println(quit.getText());
+    setTitle("Easy mode");
+    setSize(450,300);
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    setVisible(true);
+
+}
  public JPanel getEasyPanel(){
      return easyPanel;
  }
 
+
 }
+
--- src/SwingWindow.java
+++ src/SwingWindow.java
@@ -47,5 +47,6 @@ public class SwingWindow extends JFrame{
         }
     }
 
-    }
+
+}
 
