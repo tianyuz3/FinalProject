37d24cd21773933d00009035372a86d065983703
BT_4N2_06 <tzhao9389@bths.edu>
2023-05-12 12:23:10 -0400

    new

--- src/EasyMode.java
+++ src/EasyMode.java
@@ -1,21 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class EasyMode extends JFrame {
     private JPanel easyPanel;
     private JButton quit;
 
-public EasyMode(){
+    public EasyMode(){
     easyPanel.setLayout(null);
-    quit.setBounds(0,0,50,20);
-  quit.setText("Main Menu");
+        quit.setBounds(0,0,50,40);
     setContentPane(easyPanel);
-    System.out.println(quit.getText());
     setTitle("Easy mode");
     setSize(450,300);
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     setVisible(true);
+    Icon i = new ImageIcon()
+    if(quit.isEnabled()){
+        quit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SwingWindow s = new SwingWindow();
+                s.show();
+                dispose();
+                s.setSize(450, 300);
+
+            }
+        });
+    }
 
 }
  public JPanel getEasyPanel(){
